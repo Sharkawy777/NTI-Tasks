fff2b341cd334a700bad20f9e2a3c40e0bce28b5
Sharkawy777 <elsharkawy1299@gmail.com>
2022-01-02 18:17:37 +0200

    task1

---
+++ task1.php
@@ -0,0 +1,15 @@
+<?php
+
+function electricity_bill($units)
+{
+    if ($units <= 50) {
+        echo "Electricity-Bill= " . $units * 3.50;
+    } elseif ($units <= 150) {
+        echo "Electricity-Bill= " . $units * 4;
+    } else {
+        echo "Electricity-Bill= " . $units * 6.50;
+    }
+}
+
+$units = 51; // add your units here
+electricity_bill($units);
