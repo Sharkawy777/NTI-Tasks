43f3f3e195bea2089d6bd40f6b2e835283072ff0
Mahmoud El-Sharkawy <elsharkawy1299@gmail.com>
2022-01-03 09:28:42 +0200

    Update task1.php

--- task1.php
+++ task1.php
@@ -4,10 +4,10 @@ function electricity_bill($units)
 {
     if ($units <= 50) {
         echo "Electricity-Bill= " . $units * 3.50;
-    } elseif ($units <= 150) {
-        echo "Electricity-Bill= " . (50 * 3.50 + ($units-50)*4.00);
+    } elseif ($units > 50 && $units <= 150) {
+        echo "Electricity-Bill= " . (50 * 4 + ($units-50)*4.00);
     } else {
-        echo "Electricity-Bill= " . ((50 * 3.50) + (100 * 4.00) + ($units-150) * 6.50);
+        echo "Electricity-Bill= " . ((50 * 4) + (100 * 4.00) + ($units-150) * 6.50);
     }
 }
 
